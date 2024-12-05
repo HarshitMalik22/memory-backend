d479e1af7dc39965a5eb78012c4926d7c621fae7
HarshitMalik22 <130777707+HarshitMalik22@users.noreply.github.com>
2024-12-05 17:44:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -66,6 +66,6 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // Declare port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
