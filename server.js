5a99d9eb109ccbcdbe48271a6a4983647ee2b9fe
HarshitMalik22 <130777707+HarshitMalik22@users.noreply.github.com>
2024-12-06 17:17:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ connectDB();
 const cors = require('cors');
 
 const corsOptions = {
-  origin: 'https://memory-seven-flax.vercel.app/',
+  origin: 'https://memory-frontend-delta.vercel.app/',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
@@ -73,6 +73,6 @@ app.get('/', (req, res) => {
 });
 
 // Declare port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
